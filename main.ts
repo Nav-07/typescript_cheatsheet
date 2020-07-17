32c257683d2a7eef03391fca48f78f38b4336843
= <navneil2006@gmail.com>
2020-07-17 12:20:02 +1000

    Functions: Basics

---
+++ main.ts
@@ -0,0 +1,6 @@
+// Functions
+function get_sum(num1: number, num2: number): number {
+    return num1 + num2;
+} // Define the Return Type and the Parameter Type of Numbers
+
+console.log(get_sum(2, 3));
