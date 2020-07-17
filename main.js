e180a5224a2fae12e48b9d29705663a20f6e013f
= <navneil2006@gmail.com>
2020-07-17 12:08:46 +1000

    Variable Types

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 var my_string;
+var my_number; // Number can be int, float, double
+var my_boolean;
 my_string = 'Hello, From Nav!';
+my_number = 14;
+my_boolean = true;
 console.log(my_string);
+console.log(my_number);
+console.log(my_boolean);
