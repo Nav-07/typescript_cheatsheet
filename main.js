4173bda899ff663cf00cc36af3c401258f5e5fce
= <navneil2006@gmail.com>
2020-07-17 12:45:08 +1000

    Classes Done

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Class
 var Person = /** @class */ (function () {
     function Person(name, age, occupation) {
@@ -12,5 +25,24 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
-var user = new Person('Nav', 14, 'Student');
-user.greet();
+// let user = new Person('Nav', 14, 'Student');
+// user.greet();
+var Elon_Musk = /** @class */ (function (_super) {
+    __extends(Elon_Musk, _super);
+    function Elon_Musk(name, age, occupation, money) {
+        var _this = _super.call(this, name, age, occupation) || this;
+        _this.money = money;
+        return _this;
+    }
+    Elon_Musk.prototype.greet = function () {
+        _super.prototype.greet.call(this);
+        console.log(this.money);
+    };
+    Elon_Musk.prototype.show_money = function () {
+        console.log(this.money);
+    };
+    return Elon_Musk;
+}(Person));
+var musk = new Elon_Musk('Elon Musk', 45, 'Entrepreneur', 45000000000);
+musk.greet();
+musk.show_money();
--- main.ts
+++ main.ts
@@ -17,5 +17,26 @@ class Person {
     }
 }
 
-let user = new Person('Nav', 14, 'Student');
-user.greet();
+// let user = new Person('Nav', 14, 'Student');
+// user.greet();
+
+class Elon_Musk extends Person {
+    money: number;
+
+    constructor(name: string, age: number, occupation:string, money:number) {
+        super(name, age, occupation);
+        this.money = money;
+    }
+
+    greet() {
+        super.greet();
+        console.log(this.money);
+    }
+    show_money() {
+        console.log(this.money);
+    }
+}
+
+let musk = new Elon_Musk('Elon Musk', 45, 'Entrepreneur', 45000000000);
+musk.greet();
+musk.show_money();
