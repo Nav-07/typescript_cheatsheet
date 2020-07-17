efd3973f6e55b125f5be98a98e8996da5ed7248b
= <navneil2006@gmail.com>
2020-07-17 12:15:42 +1000

    Arrays

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 var my_string;
 var my_number; // Number can be int, float, double
 var my_boolean;
-my_string = 'Hello, From Nav!';
+var my_var;
+var my_string_array;
+// Same as declaring a normal array
+var new_string_array;
+var new_num_array;
+var new_bool_array;
+my_string = 'Hello, From Nav!'.slice(0, 5);
 my_number = 14;
 my_boolean = true;
+my_var = 'Nav'; // can be string or int or boolean
+my_string_array = ['Hello', 'From', 'Nav'];
 console.log(my_string);
 console.log(my_number);
 console.log(my_boolean);
+console.log(my_var);
+console.log(my_string_array);
